d235e4b675001845be05c225ea128f74fb7df3cb
Douglas Mendizabal <douglas.mendizabal@rackspace.com>
2014-02-18 18:28:51 -0600

    Delete guest user from rabbit cluster

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'cloudkeep@googlegroups.com'
 license          'Apache 2.0'
 description      'Installs/Configures barbican-rabbitmq'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.2'
+version          '0.2.3'
 
 depends          'rabbitmq'
